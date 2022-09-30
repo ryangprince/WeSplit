e7eaa156ab298e2046487ab7318c24e27fb14b6b
ryangprince <rpprince18@gmail.com>
2022-09-30 19:37:05 -0400

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ryan Prince on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
